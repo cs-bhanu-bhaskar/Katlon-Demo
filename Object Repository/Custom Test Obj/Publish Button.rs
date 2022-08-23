0644396b6d48da156e7ec8d466041988ddfdf941
Kedar <kkulkarni@fortinet.com>
2022-08-23 02:49:48 -0700

    changes commited

--- Object Repository/Custom Test Obj/Publish Button.rs
+++ Object Repository/Custom Test Obj/Publish Button.rs
@@ -6,14 +6,20 @@
    <elementGuidId>abdca1d1-bdb5-4f7c-9a45-47c036179998</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>BASIC</key>
-         <value></value>
-      </entry>
-      <entry>
          <key>XPATH</key>
          <value>//button[@id='modules-actions-publish']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <type>Main</type>
+      <value>//button[@id='modules-actions-publish2']</value>
+      <webElementGuid>ade3c5ca-0ea8-46ad-82f5-c15ebacf3419</webElementGuid>
+   </webElementXpaths>
 </WebElementEntity>
